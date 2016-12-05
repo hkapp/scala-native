0bba49c4c9cd124cd489066c021b9b0488755107
Hugo Kapp <hugo.kapp@epfl.ch>
2016-12-05 16:17:02 +0100

    Cleaner implementation for DCE

--- tools/src/main/scala/scala/scalanative/optimizer/pass/DeadCodeElimination.scala
+++ tools/src/main/scala/scala/scalanative/optimizer/pass/DeadCodeElimination.scala
@@ -9,6 +9,8 @@ import util.sh
 
 /** Eliminates pure computations that are not being used. */
 class DeadCodeElimination(implicit top: Top) extends Pass {
+  import DeadCodeElimination._
+
   override def preDefn = {
     case defn: Defn.Define =>
       //println(s"DCE: ${showGlobal(defn.name)}")
@@ -17,17 +19,20 @@ class DeadCodeElimination(implicit top: Top) extends Pass {
       val cfg      = analysis.ControlFlow.Graph(insts)
       val usedef   = analysis.UseDef(cfg)
       val newinsts = mutable.UnrolledBuffer.empty[Inst]
-      val blockParamChange = buildBlockParamChanges(cfg, usedef)
+      val paramChanges = buildParamChanges(cfg, usedef)
+      val changeCf = new ChangeApplier(paramChanges)
 
       cfg.all.foreach { block =>
         if (usedef(block.name).alive) {
-          val newLabel = changeLabel(block.label, blockParamChange(block.name))
-          newinsts += newLabel
+          //val newLabel = changeLabel(block.label, paramChanges(block.name))
+          val newParams = applyChanges(block.label.params, paramChanges(block.name))
+          //val newLabel = block.label.copy(params = )
+          newinsts += block.label.copy(params = newParams)
           block.insts.foreach {
             case inst @ Inst.Let(n, op) =>
               if (usedef(n).alive) newinsts += inst
             case inst: Inst.Cf =>
-              newinsts += changeCf(inst, blockParamChange)
+              newinsts ++= changeCf(inst)
             case _ =>
               ()
           }
@@ -37,41 +42,51 @@ class DeadCodeElimination(implicit top: Top) extends Pass {
       Seq(defn.copy(insts = newinsts))
   }
 
-  private def buildBlockParamChanges(cfg: analysis.ControlFlow.Graph, usedef: Map[Local, analysis.UseDef.Def]): Map[Local, Seq[Boolean]] = {
+  private def buildParamChanges(cfg: analysis.ControlFlow.Graph, usedef: Map[Local, analysis.UseDef.Def]): Map[Local, Seq[Boolean]] = {
     cfg.all.map { block =>
       val isEntryBlock = (block.name == cfg.entry.name)
-      val paramKept = block.params.map { param =>
+      val paramsKept = block.params.map { param =>
         (isEntryBlock || usedef(param.name).alive)
       }
-      (block.name, paramKept)
+      (block.name, paramsKept)
     }.toMap
   }
 
-  private def changeLabel(label: Inst.Label, paramChanges: Seq[Boolean]): Inst.Label = {
-    label.copy(params = applyChanges(label.params, paramChanges))
-  }
+  //private def changeLabel(label: Inst.Label, paramChanges: Seq[Boolean]): Inst.Label = {
+    //label.copy(params = applyChanges(label.params, paramChanges))
+  //}
 
-  private def changeCf(cfInst: Inst.Cf, paramChanges: Map[Local, Seq[Boolean]]): Inst.Cf = {
-    cfInst match {
-      case Inst.Jump(next) => Inst.Jump(changeNext(next, paramChanges))
-      case Inst.If(cond, thenp, elsep) => Inst.If(cond, changeNext(thenp, paramChanges), changeNext(elsep, paramChanges))
-      case _ => cfInst // the other cf insts can't have args in their next
-    }
-  }
+  //private def changeCf(cfInst: Inst.Cf, paramChanges: Map[Local, Seq[Boolean]]): Inst.Cf = {
+    //cfInst match {
+      //case Inst.Jump(next) => Inst.Jump(changeNext(next, paramChanges))
+      //case Inst.If(cond, thenp, elsep) => Inst.If(cond, changeNext(thenp, paramChanges), changeNext(elsep, paramChanges))
+      //case _ => cfInst // the other cf insts can't have args in their next
+    //}
+  //}
 
-  private def changeNext(next: Next, paramChanges: Map[Local, Seq[Boolean]]): Next = {
-    next match {
-      case Next.Label(name, args) => Next.Label(name, applyChanges(args, paramChanges(name)))
-      case _ => next
-    }
-  }
+  //private def changeNext(next: Next, paramChanges: Map[Local, Seq[Boolean]]): Next = {
+    //next match {
+      //case Next.Label(name, args) => Next.Label(name, applyChanges(args, paramChanges(name)))
+      //case _ => next
+    //}
+  //}
 
-  private def applyChanges[T](seq: Seq[T], changes: Seq[Boolean]): Seq[T] = {
-    seq.zip(changes).filter(_._2).unzip._1
-  }
 }
 
 object DeadCodeElimination extends PassCompanion {
   override def apply(config: tools.Config, top: Top) =
     new DeadCodeElimination()(top)
+
+  class ChangeApplier(paramChanges: Map[Local, Seq[Boolean]]) extends Pass {
+    override def preNext = {
+      case Next.Label(name, args) => Next.Label(name, applyChanges(args, paramChanges(name)))
+    }
+  }
+
+  def applyChanges[T](seq: Seq[T], changes: Seq[Boolean]): Seq[T] = {
+    seq.zip(changes).collect {
+      case (elem, true) => elem
+    }
+  }
+
 }
